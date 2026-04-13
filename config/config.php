5f1e86dc1277c1d2cf68a83f19eefd56ab6d2e54
juanfernandez152620 <119015733+juanfernandez152620@users.noreply.github.com>
2026-04-13 12:27:37 -0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,7 +1,17 @@
 <?php
-// Database configuration
-define('HOST', '200.58.112.193');
-define('USER', 'c2731887_api');
-define('PASSWORD', 'TUCcomp159753');
-define('DB', 'c2731887_tucucom');
+// Configuración para la clase Database principal
+    define('HOST', '200.58.112.193');
+    define('USER', 'c2731887_api');
+    define('PASSWORD', 'TUCcomp159753');
+    define('DB', 'c2731887_tucucom');
+
+    // Si planeas usar la clase Database2025 que está en tu archivo, también definí estas:
+    define('HOST-2025', '200.58.112.193');
+    define('USER-2025', 'c2731887_api');
+    define('PASSWORD-2025', 'TUCcomp159753');
+    define('DB-2025', 'c2731887_tucucom');
+
+    // Otras rutas de tu sistema
+    define('URL1', ''); 
+    define('URL', 'https://nortewalk.com.ar/');
 ?>
