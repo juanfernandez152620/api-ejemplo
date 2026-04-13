0538f286baed87c21237a7cbb348885a98b63e59
juanfernandez152620 <119015733+juanfernandez152620@users.noreply.github.com>
2026-04-13 10:56:37 -0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -6,41 +6,5 @@
     define('URLApi', 'https://www.tucumanturismo.gob.ar/');   //define constante con la direccion http para referenciar
     define('URLTurismo', 'https://www.tucumanturismo.gob.ar/');   //define constante con la direccion http para referenciar
     define('URLpc', 'https://visitatucuman.ar/');   //define constante con la direccion http para referenciar
-    //define('URL1', '/home/sentieatt484/institucionalturismotuc.gob.ar/');     //define constante con la direccion donde buscar archivos
-    //define('URL', 'https://www.institucionalturismotuc.gob.ar/');   //define constante con la direccion http para referenciar
-    //COMENTAR ESTO
-    //define('URL', 'http://10.15.15.100/MVCInstitucional/');
-    //define('URL1', 'http://10.15.15.100/MVCInstitucional/');
-
-
-    /* define('HOST', '23.229.233.154');
-    define('DB', 'turismo2016');
-    define('USER', 'touch');
-    define('PASSWORD', "ToUcH050");
-    define('CHARSET', 'utf8mb4'); */
-
-    /* DEFINICION DE CONSTANTE UTILES */
-    define('JWT-Key', 'secret');
-    
-    define('HOST', 'tucumanturismo.gob.ar');
-    define('DB', 'turismo2016');
-    define('USER', 'touch');
-    define('PASSWORD', "ToUcH050");
-    define('CHARSET', 'utf8mb4');
-
-    define('HOST-2024', 'tucumanturismo.gob.ar');
-    define('DB-2024', 'eatt2024');
-    define('USER-2024', 'adminintranet');
-    define('PASSWORD-2024', 'admin159753+');
-    define('CHARSET-2024', 'utf8mb4'); 
-    //DB 2025
-    define('HOST-2025', 'tucumanturismo.gob.ar');
-    define('DB-2025', 'eatt2025');
-    define('USER-2025', 'adminintranet');
-    define('PASSWORD-2025', 'admin159753+');
-    define('CHARSET-2025', 'utf8mb4'); 
-
-    // Definir la clave como una constante para usarla en la encriptación y desencriptación
-    define('ENCRYPTION_KEY', 'EATT8521EATT8521EATT8521EATT8521'); // 32 caracteres (256 bits)
     
 ?>
